ba38e2e2e21a108f600f1c179f37ba89016dac48
Andy Gill <andygill@ku.edu>
2010-11-24 21:47:54 -0600

    Tidy up ARQ, add better timeout guessing and random failure support.

--- Foreign/LambdaBridge/ARQ.hs
+++ Foreign/LambdaBridge/ARQ.hs
@@ -3,7 +3,17 @@
 
 -- This implements "Go-Back-N ARQ".
 
-module Foreign.LambdaBridge.ARQ where
+module Foreign.LambdaBridge.ARQ 
+	( SessionId
+	, PacketId
+	, ChannelId
+	, Data(..)
+	, Ack(..)
+	, Packet(..)
+	, ARQ_Options(..)
+	, ARQ_Protocol(..)
+	, arqProtocol
+	) where
 
 import Data.Word
 import Data.Binary
@@ -16,11 +26,12 @@ import Control.Concurrent.MVar
 import System.Timeout 
 import System.Random
 import System.IO
+import Data.Time.Clock
 
 -- 2 bit session id
 type SessionId = Word16
 type PacketId  = Word16
-type ChannelId = Word8		-- one of 255 channels (=> 256 FIFOs), 0 is controller, 1 is the main fifo.
+type ChannelId = Word8		-- ^ one of 255 channels (=> 256 FIFOs), 0 is controller, 1 is the main fifo.
 
 -- | The data packet. It is always okay to fragment a packet, as long as the packets do not appear out of order.
 -- exception: The 0 (control) channel should not be fragmented.
@@ -64,26 +75,58 @@ instance Binary Packet where
 		    _ -> return $ BadPacket
 
 
-sendWaitingForAck :: PacketId -> IO (ChannelId,BS.ByteString) -> IO Ack -> (Packet -> IO ()) -> IO ()
-sendWaitingForAck p_id takeToSendVar takeAck putSend = do
+sendWaitingForAck :: PacketId -> IO (ChannelId,BS.ByteString) -> IO Ack -> (Packet -> IO ()) -> MVar (Maybe Double) -> MVar Int -> IO ()
+sendWaitingForAck p_id takeToSendVar takeAck putSend timeoutVar timeoutTime = do
 	(chan,bs) <- takeToSendVar
-	sendWaitingForAck' p_id (Data p_id chan bs) takeToSendVar takeAck putSend
+	sendWaitingForAck' p_id (Data p_id chan bs) takeToSendVar takeAck putSend timeoutVar timeoutTime
 
-sendWaitingForAck' p_no p takeToSendVar takeAck putSend = do
+sendWaitingForAck' p_no p takeToSendVar takeAck putSend timeoutVar timeoutTime = do
 	-- send the packet
 	putSend (DataPacket p)
 
+	tm  <- readMVar timeoutTime
+	tm0 <- getCurrentTime
 	-- wait for the awk (ignoring other awks)
-	getAck <- timeout 100000 $ let
+	getAck <- timeout tm $ let
 		loop = do
-		   (Ack ack) <- takeAck
-		   if ack == p_no then return () else loop in loop
-
+		   Ack ack <- takeAck
+		   if ack == p_no
+			then return ()
+			else loop
+	    in loop
+	-- be careful, http://en.wikipedia.org/wiki/Sorcerer%27s_Apprentice_Syndrome
 	case getAck of
 		-- timeout happened, try send packet again
-	   Nothing -> sendWaitingForAck' p_no p takeToSendVar takeAck putSend
+	   Nothing -> do putMVar timeoutVar Nothing
+			 sendWaitingForAck' p_no p takeToSendVar takeAck putSend timeoutVar timeoutTime
 		-- done, move on please
-	   Just () -> sendWaitingForAck (p_no + 1) takeToSendVar takeAck putSend
+	   Just () -> do tm1 <- getCurrentTime
+			 putMVar timeoutVar $ Just (realToFrac $ tm1 `diffUTCTime` tm0) 
+		         sendWaitingForAck (p_no + 1) takeToSendVar takeAck putSend timeoutVar timeoutTime
+
+
+handleTimeouts :: MVar (Maybe Double) -> IO (MVar Int)
+handleTimeouts timings = do
+	let firstTime = 1 :: Double	-- 1 second max latency (0.5 each direction)
+	var <- newEmptyMVar
+	let loop avr = do
+		tryTakeMVar var
+		putMVar var (floor (avr * 1000 * 1000))	-- av is the best guess so far for a good timeout
+		v <- takeMVar timings
+{-
+		case v of
+		  Nothing -> print avr
+		  Just {} -> return ()
+-}
+		case v of
+			-- Timeout happend, blame the timeout, double the timeout
+		   Nothing -> loop (min (avr * 2) firstTime)
+			-- Wait at least the average of 4 times average length of this packet and the old timeout.
+			-- This stops one single fast (mistaken) round trip time from messing things up
+		   Just t  -> loop ((t * 4 + avr) / 2)
+	forkIO $ loop firstTime
+	return var
+
 
 recvReplyWithAck :: PacketId -> IO Data -> (Packet -> IO ()) -> ((ChannelId,BS.ByteString) -> IO ()) -> IO ()
 recvReplyWithAck n takeRecvVar putSendVar putHaveRecvVar = do
@@ -97,32 +140,27 @@ recvReplyWithAck n takeRecvVar putSendVar putHaveRecvVar = do
 		     -- Todo: consider Nack		
 		  else do recvReplyWithAck n takeRecvVar putSendVar putHaveRecvVar
 
-data RandomErrors = NoRandomErrors 
-		  | RandomErrors StdGen Float	-- The generator, and how often packets suceed (0.90 is a good number)
-
-sendBits :: RandomErrors -> IO Packet -> (BS.ByteString -> IO ()) -> IO ()
-sendBits err takeSendVar putSockVar = do
+sendBits :: [Bool] -> IO Packet -> (BS.ByteString -> IO ()) -> IO ()
+sendBits (True:errs) takeSendVar putSockVar = do
+	packet <- takeSendVar
+	sendBits errs takeSendVar putSockVar
+sendBits (False:errs) takeSendVar putSockVar = do
 	packet <- takeSendVar
 	let bs = BS.concat $ LBS.toChunks $ encode packet
-	case err of
-	  NoRandomErrors -> do 
-		putSockVar bs
-		sendBits err takeSendVar putSockVar
-	  RandomErrors g cmp -> do
-		let (b,g') = random g
-		if (b :: Float) >= cmp then return () else putSockVar bs
-		sendBits (RandomErrors g' cmp) takeSendVar putSockVar
+	putSockVar bs
+	sendBits errs takeSendVar putSockVar
 
-
-recvBits :: RandomErrors -> IO BS.ByteString -> (Data -> IO ()) -> (Ack -> IO ()) -> IO ()
-recvBits err takeRecv putRecvVar putAckVar = do
+recvBits :: [Bool] -> IO BS.ByteString -> (Data -> IO ()) -> (Ack -> IO ()) -> IO ()
+recvBits (True:errs) takeRecv putRecvVar putAckVar = do
+	bs <- takeRecv
+	recvBits errs takeRecv putRecvVar putAckVar
+recvBits (False:errs) takeRecv putRecvVar putAckVar = do
 	bs <- takeRecv
 	case decode (LBS.fromChunks [bs]) of
-	  DataPacket dat -> do putRecvVar dat
-	  AckPacket ack  -> do putAckVar ack
-	  _ -> do hPutStrLn stderr "badly formed packet received (ignoring)"
-		  return ()
-	recvBits err takeRecv putRecvVar putAckVar 
+	  	DataPacket dat -> do putRecvVar dat
+	  	AckPacket ack  -> do putAckVar ack
+	  	_ -> do return ()	-- ignore this packet
+	recvBits errs takeRecv putRecvVar putAckVar 
 
 data ARQ_Options = ARQ_Options
 	{ toSocket 	  :: BS.ByteString -> IO ()	-- 
@@ -138,25 +176,36 @@ data ARQ_Protocol = ARQ_Protocol
 	
 arqProtocol :: ARQ_Options -> IO ARQ_Protocol
 arqProtocol opts = do
-	let rnd = NoRandomErrors
+	seed <- getStdGen
+	let (s1,s2) = split seed
+	let sendFailures = case transmitFailure opts of
+		 	    Nothing -> repeat False
+			    Just f -> [ n < f | n <- randoms s1 ]
+	let recvFailures = case receiveFailure opts of
+		 	    Nothing -> repeat False
+			    Just f -> [ n < f | n <- randoms s2 ]
 	toSendVar <- newEmptyMVar :: IO (MVar (ChannelId,BS.ByteString))
 	sendVar <- newEmptyMVar :: IO (MVar Packet)
 	ackVar  <- newEmptyMVar :: IO (MVar Ack)
 	recvVar <- newEmptyMVar :: IO (MVar Data)
 	haveRecvVar <- newEmptyMVar :: IO (MVar (ChannelId,BS.ByteString))
+	timeoutVar <- newEmptyMVar :: IO (MVar (Maybe Double))
+	timeoutTime <- handleTimeouts timeoutVar
 
 		-- This waits for awk's when sending packets
 	forkIO $  sendWaitingForAck 0
 				(takeMVar toSendVar)
 				(takeMVar ackVar)
 				(putMVar sendVar)
+				timeoutVar
+				timeoutTime
 
 		-- This connects the "sendVar" MVar with the outgoing connection.
-	forkIO $ sendBits rnd (takeMVar sendVar) (toSocket opts)
+	forkIO $ sendBits sendFailures (takeMVar sendVar) (toSocket opts)
 		
 
 		-- This send bits over the connection
-	forkIO $ recvBits rnd
+	forkIO $ recvBits recvFailures
 			     (fromSocket opts)
 			     (putMVar recvVar)
 			     (putMVar ackVar)
