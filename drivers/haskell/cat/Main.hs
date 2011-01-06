1c2b548538f04d18e76ff043cf6a8d5ae9a2392a
Andy Gill <andygill@ku.edu>
2011-01-06 10:19:37 -0600

    Fix cat to work

--- drivers/haskell/cat/Main.hs
+++ drivers/haskell/cat/Main.hs
@@ -7,6 +7,6 @@ import Network.LambdaBridge.Driver
 -- Trivial driver, just reflects back the input. Assumes one in, one out.
 main = bundle_driver "cat" $ \ args ins outs ->
 	case (args,ins,outs) of
-	  ([],[h1],[h2]) -> forever $ do
+	  (opt,[h1],[h2]) -> forever $ do
 		c <- hGetChar h1
 		hPutChar h2 c
--- examples/haskell/cat/Main.hs
+++ examples/haskell/cat/Main.hs
@@ -9,15 +9,24 @@ module Main where
 
 import Network.LambdaBridge
 import System.IO
+import Control.Concurrent
+import Control.Monad
 
 main = do
 	putStrLn "Connecting to 'cat' lambda bridge"
 	([send],[recv]) <- board_connect (1,1) ["lb_cat","--debug"] 
 
-	hPutStrLn send "Hello, World!"
-	hFlush send
-	str <- hGetLine recv
-	putStrLn str
+
+	let loop n = do
+		hPutStrLn send $ "Message " ++ show n ++ "!"
+		hFlush send
+		loop (n+1)
+
+	forkIO $ loop 0
+
+	forever $ do 
+		str <- hGetLine recv
+		putStrLn str
 
 	putStrLn "Exiting lambda bridge"
 
