12754d546915bbe9b5145fa0361a080c61bcc9c2
Andy Gill <andygill@ku.edu>
2011-01-05 23:01:43 -0600

    Adding cat driver in Haskell.

---
+++ drivers/cat/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+	
+import System.IO
+import Control.Monad
+import Network.LambdaBridge.Driver
+
+-- Trivial driver, just reflects back the input. Assumes one in, one out.
+main = bundle_driver "cat" $ \ args ins outs ->
+	case (args,ins,outs) of
+	  ([],[h1],[h2]) -> forever $ do
+		c <- hGetChar h1
+		hPutChar h2 c
