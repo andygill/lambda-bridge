da3f6318cc26e288a64a1d6f5da20eb037f0afd6
Andy Gill <andygill@ku.edu>
2011-10-25 11:46:54 -0500

    stub out lb_cat

--- drivers/haskell/cat/Main.hs
+++ drivers/haskell/cat/Main.hs
@@ -2,11 +2,16 @@ module Main where
 	
 import System.IO
 import Control.Monad
-import Network.LambdaBridge.Driver
+
+-- import Network.LambdaBridge.Sockets
 
 -- Trivial driver, just reflects back the input. Assumes one in, one out.
-main = bundle_driver "cat" $ \ args ins outs ->
+main = error ""
+{-
+main = openServer "
+        bundle_driver "cat" $ \ args ins outs ->
 	case (args,ins,outs) of
 	  (opt,[h1],[h2]) -> forever $ do
 		c <- hGetChar h1
 		hPutChar h2 c
+-}
