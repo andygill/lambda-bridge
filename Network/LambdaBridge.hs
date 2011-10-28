3f4d80b8aa9618d8019d35248bafc4b7c041af7d
Andy Gill <andygill@ku.edu>
2011-10-28 00:22:10 -0500

    Clean up the main lambda-bridge entry point.

--- Network/LambdaBridge.hs
+++ Network/LambdaBridge.hs
@@ -1,48 +1,18 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module Network.LambdaBridge (simple_board_connect, board_connect) where
+module Network.LambdaBridge (connect, SocketName) where
 
-import Foreign.C
-import Foreign.Ptr
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Array
-import Foreign.Storable
-import GHC.IO.Handle.FD
 import System.IO
+import Network.LambdaBridge.Socket
 
+-- | 'connect' connects to a FPGA, via a pre-initialized lambda-bridge driver.
+-- This is the sole user-facing side of lambda-bridge, all other
+-- libraries are for engineering building drivers for lambda-bridge.
+--
+-- Note that connect is a (almost trivial) alias of connectTo from the network library.
+-- There is no reason why, if prefered, the standard network libary,
+-- or another socket opening library can not be used instead to open the socket.
+-- It is provided for completeness and convenience.
 
--- | 'simple_board_connect' connects to a FPGA, via a specified lambda-bridge driver.
-
-simple_board_connect :: [String] -> IO (Handle,Handle)
-simple_board_connect argv = do
-	(sends,recvs) <- board_connect (1,1) argv
-	case (sends,recvs) of
-	  ([s],[r]) -> return (s,r)
-	  _ -> error $ "simple_board_connect failed, wrong number of channels opened"
-
--- | 'board_connect' connects to a FPGA, via a specified lambda-bridge driver,
---  and allows for multiple SEND and RECV channels.
-
-board_connect :: (Int,Int) -> [String] -> IO ([Handle],[Handle])
-board_connect (1,1) argv = do
-	let args = "haskell-lambda-bridge" : argv
-	argv_ptrs <- sequence
-		[ newCString arg
-		| arg <- args
-		]
-	argv_ptr <- newArray0 nullPtr argv_ptrs
-	hds_ptr <- mallocArray 2
-	res <- lb_board_connect (fromIntegral $ length args)
-				 argv_ptr
-				 1
-				 1
-				 hds_ptr
-	hds <- sequence [ do i <- peekElemOff hds_ptr i
-	 		     fdToHandle i 
-	                | i <- take 2 [0..]]
-	sequence_ [ hSetBuffering hd NoBuffering | hd <- hds ]
-	return ([hds !! 0],[hds !! 1])
-board_connect _ _ = error $ "board_connect: only single FIFOs in each direction are currently supported"
-
-foreign import ccall unsafe "lb_board_connect.h lb_board_connect" lb_board_connect 
-	:: CInt -> Ptr (Ptr CChar) -> CInt -> CInt -> Ptr CInt -> IO CInt
+connect :: SocketName -> IO Handle
+connect = openAsClient
