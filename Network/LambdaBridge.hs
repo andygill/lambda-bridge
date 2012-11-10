52eab5e02dbac6d3d08c8cfacf463cb0c2b105d1
Andy Gill <andygill@ku.edu>
2012-11-10 16:15:47 -0600

    Fixing things for new repo.

--- Network/LambdaBridge.hs
+++ Network/LambdaBridge.hs
@@ -1,18 +1,5 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
-module Network.LambdaBridge (connect, SocketName) where
+module Network.LambdaBridge  where
 
-import System.IO
-import Network.LambdaBridge.Socket
-
--- | 'connect' connects to a FPGA, via a pre-initialized lambda-bridge driver.
--- This is the sole user-facing side of lambda-bridge, all other
--- libraries are for engineering building drivers for lambda-bridge.
---
--- Note that connect is a (almost trivial) alias of connectTo from the network library.
--- There is no reason why, if prefered, the standard network libary,
--- or another socket opening library can not be used instead to open the socket.
--- It is provided for completeness and convenience.
-
-connect :: SocketName -> IO Handle
-connect = openAsClient
+-- Nothing right now
